4d59373c2fb24af4485c649819d3c051cb105c2f
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-07-01 17:20:04 -0600

    Adding a navigation prop and touchable opacity to images

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,7 +5,7 @@ import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = () => {
+const SearchScreen = ({navigation}) => {
   // console.log('hi there');
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
@@ -30,11 +30,17 @@ const SearchScreen = () => {
         <ResultsList
           results={filterResultsByPrice('$')}
           title="Cost Effective"
+          navigation={navigation}
         />
-        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice('$$')}
+          title="Bit Pricier"
+          navigation={navigation}
+        />
         <ResultsList
           results={filterResultsByPrice('$$$')}
           title="Big Spender"
+          navigation={navigation}
         />
       </ScrollView>
     </View>
